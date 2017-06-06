68aaec70357dc58d351ad4250a38dea4f8e83a7c
KostaNau <naumov.k@gmail.com>
2017-06-06 16:26:20 +0300

    Initial commit

--- README.md
+++ README.md
@@ -1,6 +1,27 @@
 # Night Owls Detector
 
-[TODO. There will be project description]
+## Description
+The script shows pupils of education project [DEVMAN.org](https://devman.org) who send commits with solutions after midnight acoording pupil's local time. 
+
+
+## How to use
+Run the script ```python3 seek_dev_nighters.py```.
+By default the script determine night owls whose commits have been sent between 12am and 6am. 
+You could define other time for night shift use optional key `-n`
+
+### Example
+```python3 seek_dev_nighters.py -n 5```
+
+```
+Midnighter: rancvova. Amount attempts after midnight: 5
+Midnighter: mikhail.ushanov. Amount attempts after midnight: 5
+Midnighter: alexander.i.kamenev. Amount attempts after midnight: 3
+```
+
+### Requirements
+Install the dependencies from requirements.txt using pip:
+
+```pip install -r requirements.txt```
 
 # Project Goals
 
--- seek_dev_nighters.py
+++ seek_dev_nighters.py
@@ -1,15 +1,78 @@
-def load_attempts():
-    pages = 1
-    for page in range(pages):
-        # FIXME подключить загрузку данных из API
-        yield {
-            'username': 'bob',
-            'timestamp': 0,
-            'timezone': 'Europe/Moscow',
-        }
+import argparse
+import textwrap
+from datetime import datetime, time
+from collections import Counter
 
-def get_midnighters():
-    pass
+import requests
+from pytz import timezone
+
+
+def load_attempts() -> dict:
+    api_url = "https://devman.org/api/challenges/solution_attempts"
+    pages = requests.get(api_url).json()["number_of_pages"]
+    for page in range(1, pages + 1):
+        user_data = requests.get(api_url, params={"page": page}).json()
+        yield from user_data["records"]
+
+
+def is_after_midnignt(timestamp: str,
+                      usr_timezone: str,
+                      night_shift: int) -> bool:
+
+    attempt_timestamp = timestamp
+    if attempt_timestamp:
+        attempt_time = datetime.fromtimestamp(
+                            attempt_timestamp, timezone(usr_timezone)).time()
+
+        return time(0, 0) < attempt_time < time(night_shift, 0)
+
+
+def get_midnighters(night_shift: int) -> list:
+    user_and_attempts = []
+    for record in load_attempts():
+        user_name = record["username"]
+        attempt_time = is_after_midnignt(
+                                         record["timestamp"],
+                                         record["timezone"],
+                                         night_shift)
+        if attempt_time:
+            user_and_attempts.append(user_name)
+    midnighters = Counter(user_and_attempts).most_common()
+    return midnighters
+
+
+def parse_arg() -> argparse:
+    parser = argparse.ArgumentParser(
+                formatter_class=argparse.RawDescriptionHelpFormatter,
+                description=textwrap.dedent('''\
+                                    *** Night Owls Detector ***
+            -------------------------------------------------------------------
+            |   The script shows pupils of education project DEVMAN.org who   |
+            |   send commits with solutions after midnight local time.        |
+            -------------------------------------------------------------------
+                                            '''
+                                            )
+                                )
+    parser.add_argument("-n", "--nightshift",
+                        type=int,
+                        default=6,
+                        help="Night shift length (by default 6 hrs, 12am-6am)"
+                        )
+    return parser.parse_args().nightshift
+
+
+def console_output(midnighters: list) -> None:
+    for midnighter in midnighters:
+        print("Midnighter: {}. Amount attempts after midnight: {}".
+              format(midnighter[0], midnighter[1]))
+
+
+def main():
+    night_shift = parse_arg()
+    midnighters = get_midnighters(night_shift)
+    console_output(midnighters)
+
 
 if __name__ == '__main__':
-  pass
+
+    main()
